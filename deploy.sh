8ed8e0d1177487b7f1d8b30cec075d2dbe6bc018
Pierre Gueth <pierre.gueth@gmail.com>
2025-09-09 12:56:35 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+PROJECT="spider"
+DEMO_DIR="../demo_public/$PROJECT"
+WASM_PATH="$(find "target" | grep "$PROJECT.wasm")"
+
+if [ ! -d "$DEMO_DIR" ]; then
+  echo "$DEMO_DIR is not a directory."
+  exit
+fi
+if [ ! -f "$WASM_PATH" ]; then
+  echo "$WASM_PATH is not file."
+  exit
+fi
+
+echo "demo $DEMO_DIR"
+echo "wasm $WASM_PATH"
+
+wasm-bindgen "$WASM_PATH" --target web --no-typescript --out-dir "$DEMO_DIR"
+cp -r "assets" "$DEMO_DIR/assets"
+cp "index.html" "$DEMO_DIR/index.html"
