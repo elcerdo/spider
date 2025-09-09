791d2eb0d8c2cd0da46cb1843446127a4191163d
Pierre Gueth <pierre.gueth@gmail.com>
2025-09-09 11:55:40 +0200

    play animation

--- src/spider/mod.rs
+++ src/spider/mod.rs
@@ -1,13 +1,17 @@
-use bevy::color::palettes::css::*;
-use bevy::prelude::*;
-
 mod data;
 mod physics;
 
 use data::SpiderData;
 
+use bevy::scene::SceneInstanceReady;
+
+use bevy::color::palettes::css::*;
+use bevy::prelude::*;
+
 const MODEL_SPIDER_PATH: &str = "models/tachikoma.glb";
 // const MODEL_SPIDER_SCALE: f32 = 1.0;
+
+//////////////////////////////////////////////////////////////////////
 
 pub struct SpiderPlugin;
 
@@ -32,9 +36,6 @@ impl Plugin for SpiderPlugin {
     }
 }
 
-// #[derive(Default, Reflect, GizmoConfigGroup)]
-// struct MyRoundGizmos;
-
 //////////////////////////////////////////////////////////////////////
 
 // fn exit_game(mut next_state: ResMut<NextState<GlobalState>>, keyboard: Res<ButtonInput<KeyCode>>) {
@@ -56,21 +57,37 @@ fn reset_vehicle_positions(
 
 //////////////////////////////////////////////////////////////////////
 
+#[derive(Component)]
+struct SpiderAnimation {
+    graph: Handle<AnimationGraph>,
+    index: AnimationNodeIndex,
+}
+
 fn populate_spider(
     mut commands: Commands,
     server: Res<AssetServer>,
     mut gizmo_assets: ResMut<Assets<GizmoAsset>>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
     // mut materials: ResMut<Assets<StandardMaterial>>,
     // tracks: Res<Assets<Track>>,
     // state: Res<State<GlobalState>>,
 ) {
+    // animation from our example asset, which has an index of two.
+    let (graph, index) = AnimationGraph::from_clip(
+        server.load(GltfAssetLabel::Animation(0).from_asset(MODEL_SPIDER_PATH)),
+    );
+    let graph: Handle<AnimationGraph> = graphs.add(graph);
+
     let scene: Handle<Scene> = server.load(GltfAssetLabel::Scene(0).from_asset(MODEL_SPIDER_PATH));
 
     let mut scene = commands.spawn((
         SceneRoot(scene.clone()),
         SpiderData::from_position(Vec2::ZERO),
+        SpiderAnimation { graph, index },
         Transform::from_translation(Vec3::Z * 5.0),
     ));
+
+    scene.observe(play_animation_when_ready);
 
     scene.with_children(|parent| {
         let mut gizmo = GizmoAsset::new();
@@ -92,6 +109,42 @@ fn populate_spider(
             ..default()
         });
     });
+}
+
+fn play_animation_when_ready(
+    trigger: Trigger<SceneInstanceReady>,
+    mut commands: Commands,
+    animations: Query<&SpiderAnimation>,
+    children: Query<&Children>,
+    mut players: Query<&mut AnimationPlayer>,
+) {
+    info!("** starting animation **");
+
+    // The entity we spawned in `setup_mesh_and_animation` is the trigger's target.
+    // Start by finding the AnimationToPlay component we added to that entity.
+    let target = trigger.target();
+    if let Ok(animation) = animations.get(target) {
+        // The SceneRoot component will have spawned the scene as a hierarchy
+        // of entities parented to our entity. Since the asset contained a skinned
+        // mesh and animations, it will also have spawned an animation player
+        // component. Search our entity's descendants to find the animation player.
+        for child in children.iter_descendants(target) {
+            if let Ok(mut player) = players.get_mut(child) {
+                // Tell the animation player to start the animation and keep
+                // repeating it.
+                //
+                // If you want to try stopping and switching animations, see the
+                // `animated_mesh_control.rs` example.
+                player.play(animation.index).repeat();
+
+                // Add the animation graph. This only needs to be done once to
+                // connect the animation player to the mesh.
+                commands
+                    .entity(child)
+                    .insert(AnimationGraphHandle(animation.graph.clone()));
+            }
+        }
+    }
 }
 
 fn update_gizmos(
