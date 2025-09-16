9d0fbda4406d188b91b09c72275604b596526d0c
Pierre Gueth <pierre.gueth@gmail.com>
2025-09-16 21:33:14 +0200

    still plays idle

--- src/spider/anim.rs
+++ src/spider/anim.rs
@@ -8,25 +8,19 @@ pub fn populate_animations(
     trigger: Trigger<SceneInstanceReady>,
     animations: Query<&SpiderAnimation>,
     children: Query<&Children>,
+    players: Query<&AnimationPlayer>,
     mut commands: Commands,
-    mut players: Query<&mut AnimationPlayer>,
 ) {
     info!("** populate animations **");
 
     let target = trigger.target();
     if let Ok(animation) = animations.get(target) {
         for child in children.iter_descendants(target) {
-            if let Ok(mut player) = players.get_mut(child) {
-                // Tell the animation player to start the animation and keep
-                // repeating it.
-                //
-                // If you want to try stopping and switching animations, see the
-                // `animated_mesh_control.rs` example.
-                // player.play(animation.index_idle).repeat();
-                // player.play(animation.weighted_nodes[0].1);
-
-                // Add the animation graph. This only needs to be done once to
-                // connect the animation player to the mesh.
+            if players.get(child).is_ok() {
+                warn!(
+                    "inserting graph with {} weighted nodes",
+                    animation.weighted_nodes.len()
+                );
                 commands
                     .entity(child)
                     .insert(AnimationGraphHandle(animation.graph.clone()));
@@ -53,34 +47,16 @@ pub fn update_weights(
 
                     // If the animation happens to be no longer active, restart it.
                     if !player.is_playing_animation(node) {
-                        warn!("start {:?}", node);
+                        warn!("starting node {:?}", node);
                         player.play(node).repeat();
                     }
 
                     // Set the weight.
                     if let Some(active_animation) = player.animation_mut(node) {
-                        info!("set weight {}", weight);
                         active_animation.set_weight(*weight);
                     }
                 }
             }
         }
     }
-
-    // for (mut player, animation) in query.iter_mut() {
-    //     for (weight, node) in animation.weighted_nodes.iter() {
-    //         let node = *node;
-
-    //         // If the animation happens to be no longer active, restart it.
-    //         if !player.is_playing_animation(node) {
-    //             info!("restart {:?}", node);
-    //             player.play(node);
-    //         }
-
-    //         // Set the weight.
-    //         if let Some(active_animation) = player.animation_mut(node) {
-    //             info!("set weight {}", weight);
-    //             active_animation.set_weight(*weight);
-    //         }
-    //     }
 }
